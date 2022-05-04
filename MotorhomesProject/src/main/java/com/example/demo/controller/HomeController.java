f7308c517489c2b7a0f69ce29c85bd1240e4e98b
A-Stroem <asjnyborg@hotmail.com>
2022-05-04 22:30:25 +0200

    index++

--- MotorhomesProject/src/main/java/com/example/demo/controller/HomeController.java
+++ MotorhomesProject/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,7 +1,18 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
 
 @Controller
 public class HomeController {
+
+
+    @GetMapping("/")
+    public String frontPage(Model model) {
+        return "frontpage/index";
+    }
+
 }
