ba65f3b6ca5de955b19768317770ea655ca35019
moniozd <ozdobamonika@outlook.com>
2022-05-07 07:38:36 +0200

    accessories, motorhomes repositories

--- MotorhomesProject/src/main/java/com/example/demo/repository/AccessoryRepository.java
+++ MotorhomesProject/src/main/java/com/example/demo/repository/AccessoryRepository.java
@@ -1,11 +1,21 @@
 package com.example.demo.repository;
 
 
+import com.example.demo.model.CustomerModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class AccessoryRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
-
-
+    public List<CustomerModel> fetchAll(){
+        return jdbcTemplate.query("SELECT * FROM accessories;",
+                new BeanPropertyRowMapper<>(CustomerModel.class));
+    }
 }
